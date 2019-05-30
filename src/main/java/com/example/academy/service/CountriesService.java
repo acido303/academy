4dcb37167357f1905ada971aa27c1ad0d15546f5
José Antonio Sánchez <acido303@gmail.com>
2019-05-30 17:18:02 +0100

    Added Service for Countries

--- src/main/java/com/example/academy/service/CountriesService.java
+++ src/main/java/com/example/academy/service/CountriesService.java
@@ -1,4 +1,25 @@
 package com.example.academy.service;
 
+import com.example.academy.model.Countries;
+import com.example.academy.repository.CountriesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+
+@Service
 public class CountriesService {
+
+    private final CountriesRepository countriesRepository;
+
+    public CountriesService(CountriesRepository countriesRepository) {
+        this.countriesRepository = countriesRepository;
+    }
+
+    public List<Countries> findAll(){
+        List<Countries> countries = countriesRepository.findAll();
+        countries.sort(Comparator.comparing(Countries::getName));
+        return countries;
+    }
 }
