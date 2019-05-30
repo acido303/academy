f2b77ae358461e246e696d564ccff3f155368ff5
José Antonio Sánchez <acido303@gmail.com>
2019-05-30 17:40:06 +0100

    Added UsersService

---
+++ src/main/java/com/example/academy/service/UsersService.java
@@ -0,0 +1,23 @@
+package com.example.academy.service;
+
+import com.example.academy.model.Users;
+import com.example.academy.repository.UsersRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> findAll(){
+        return usersRepository.findAll();
+    }
+
+
+}
